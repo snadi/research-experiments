4fa84d2021aa3c213fb5ee585dc1b85b66348a47
Sarah Nadi <nadi@ualberta.ca>
2023-09-14 11:38:22 -0600

    Don't separate by lang

--- .github/parse_repos.py
+++ .github/parse_repos.py
@@ -3,22 +3,23 @@ import logging as log
 import json
 
 def get_repos():
-    repos = {}
+    repos = []
     script_dir = os.path.dirname(__file__)
     java_repo_file = os.path.join(script_dir, "java-repo-list.txt")
     python_repo_file = os.path.join(script_dir, "python-repo-list.txt")
 
-    repos["java"] = parse_repo_file(java_repo_file)
-    repos["python"] = parse_repo_file(python_repo_file)
+    repos.extend(parse_repo_file(java_repo_file, lang='java'))
+    repos.extend(parse_repo_file(python_repo_file, lang='python'))
     return repos
 
-def parse_repo_file(repo_file):
+def parse_repo_file(repo_file, lang):
     repos =[]
     with open(repo_file, 'r') as jfile:
         for line in jfile:
             reponame, commit = line.strip().split('#')
             reponame = reponame.split(':')[-1]
-            repos.append({"repo": reponame, "commit": commit})          
+            reponame = reponame.split('.git'[0])
+            repos.append({"repo": reponame, "commit": commit, "lang": lang})          
     return repos
 
 if __name__ == "__main__":
